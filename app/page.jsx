07671e308604ec68313af9f0b20da8291116d005
Richard Ramos <richardramos112094@gmail.com>
2024-05-07 08:39:34 -0400

    updated3

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-
-
-
+import { redirect } from 'next/navigation'
 
 export default function Home() {
+  redirect(`/congo`) // Navigate to the new post page
+
   return (
     <main className="main">
-      API
+      <h1>Redirecting...</h1>
     </main>
   );
 }
