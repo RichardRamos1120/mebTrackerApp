7b990d2748f11823d6eaaa0f71d65fad015b3229
Richard Ramos <38115103+RichardRamos1120@users.noreply.github.com>
2024-05-15 01:51:55 +0800

    Update page.jsx

--- app/congo/page.jsx
+++ app/congo/page.jsx
@@ -6,7 +6,7 @@ const Page = async () => {
     const currentDate = new Date();
     const formattedDate = currentDate.toDateString();
 
-    const res = await fetch('https://85c9-120-29-78-26.ngrok-free.app/api/congo',{
+    const res = await fetch('http://137.184.224.29:3001/api/congo',{
         next: { revalidate: 0 },
       });
 
